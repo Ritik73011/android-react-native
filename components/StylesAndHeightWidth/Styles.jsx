2b21aed0a7b46378a352db6b7cbf5505bbb7b51f
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-03-01 13:14:53 +0530

    styles in react-native

--- components/StylesAndHeightWidth/Styles.jsx
+++ components/StylesAndHeightWidth/Styles.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TextInput } from "react-native";
 const Styles = () => {
   return (
     <>
@@ -9,7 +9,9 @@ const Styles = () => {
       </View>
       <View style={styles.parent2}>
         <View style={styles.view1}></View>
-        <View style={styles.view2}></View>
+        <View style={styles.view2}>
+          <TextInput placeholder="Your good name" style={styles.inputBox} />
+        </View>
         <View style={styles.view3}></View>
       </View>
     </>
@@ -36,10 +38,14 @@ const styles = StyleSheet.create({
   view2: {
     flex: 1,
     backgroundColor: "skyblue",
+    alignItems: "center",
   },
   view3: {
     flex: 1,
     backgroundColor: "lightgreen",
+  },
+  inputBox: {
+    width: "100%",
   },
 });
 export default Styles;
