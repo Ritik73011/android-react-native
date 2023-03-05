2c68ed9e8ca5157ee100bb9ff3c56b400962eb53
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-03-05 14:39:04 +0530

    screen navigator

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import Styles from './components/StylesAndHeightWidth/Styles';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  Button, Text, View,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  TouchableWithoutFeedback,
+  Alert,
+} from 'react-native';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar />
-      <Styles />
-    </>
+      <Stack.Navigator>
+        <Stack.Screen name='home'
+          component={Home}
+          options={{ title: "Home" }} />
+
+        <Stack.Screen name='style'
+          component={Styles}
+          options={{ title: "own styles" }} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+}
+
+const Home = ({ navigation }) => {
+  return <View style={{ flex: 1 }}>
+    <Button
+      title='Styles'
+      onPress={() => { navigation.navigate('style', { placeholder: "Your good name..." }) }} />
+    <View style={{ flex: 1, flexDirection: "column", justifyContent: "space-between", alignItems: "center" }}>
+      <TouchableHighlight onPress={() => Alert.alert("TouchableHighlight")}>
+        <View style={{ backgroundColor: "pink", padding: 10 }}>
+          <Text>TouchableHighlight</Text>
+        </View>
+      </TouchableHighlight>
+
+      <TouchableOpacity onLongPress={() => Alert.alert("TouchableOpacity")}>
+        <View style={{ backgroundColor: "red", padding: 10 }}>
+          <Text>TouchableOpacity</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableNativeFeedback onPress={() => Alert.alert("TouchableNativeFeedback")}>
+        <View style={{ backgroundColor: "yellow", padding: 10 }}>
+          <Text>TouchableNativeFeedback</Text>
+        </View>
+      </TouchableNativeFeedback>
+
+      <TouchableWithoutFeedback onPress={() => Alert.alert("TouchableWithoutFeedback")}>
+        <View style={{ backgroundColor: "lightgreen", padding: 10 }}>
+          <Text>TouchableWithoutFeedback</Text>
+        </View>
+      </TouchableWithoutFeedback>
+
+    </View>
+  </View>
 }
