e9655a50186d3de6efc48c754081ce11f882ea1e
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-03-06 13:10:24 +0530

    working with flat-list

---
+++ components/FlatLists/FlatLists.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const FlatLists = () => {
+  return (
+    <View>
+      <Text>FlatList</Text>
+    </View>
+  );
+};
+
+export default FlatLists;
