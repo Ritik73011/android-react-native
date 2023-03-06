2bbbfaf5feb1f6ee499536f3fe819f9fa195b2ca
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-03-06 14:18:37 +0530

    working with flatlists

--- components/FlatLists/FlatLists.jsx
+++ components/FlatLists/FlatLists.jsx
@@ -1,12 +1,57 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView, FlatList } from "react-native";
 
-const FlatLists = () => {
+const data = [
+  { num: 1 },
+  { num: 2 },
+  { num: 3 },
+  { num: 4 },
+  { num: 5 },
+  { num: 6 },
+  { num: 7 },
+  { num: 8 },
+  { num: 9 },
+  { num: 10 },
+  { num: 11 },
+  { num: 12 },
+  { num: 13 },
+  { num: 14 },
+  { num: 15 },
+  { num: 16 },
+];
+
+const SingleItem = ({ num }) => {
   return (
-    <View>
-      <Text>FlatList</Text>
+    <View style={styles.singleView}>
+      <Text>{num}</Text>
     </View>
   );
 };
+const FlatLists = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <SingleItem num={item.num} />}
+        keyExtractor={(item) => item.num}
+        numColumns={2}
+      />
+    </SafeAreaView>
+  );
+};
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  singleView: {
+    padding: 10,
+    backgroundColor: "yellow",
+    margin: "2%",
+    alignItems: "center",
+    width: "46%",
+    height: 90,
+    justifyContent: "center",
+  },
+});
 export default FlatLists;
